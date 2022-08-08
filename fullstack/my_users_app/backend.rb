e66808f4c5d8d5570a68ea3015f200935fbb8599
ctcmc <ccab0528@gmail.com>
2022-08-08 16:49:12 -0700

    write routes, create more class methods

--- fullstack/my_users_app/backend.rb
+++ fullstack/my_users_app/backend.rb
@@ -1,8 +1,119 @@
 require 'sinatra'
-#require './my_user_model.rb'
+require 'sqlite3'
+set :port, 8000
+enable :sessions
 
+
+db = SQLite3::Database.new "db.sql"
+
+users = db.execute <<-SQL
+    create table if not exists users (
+        firstname varchar(15),
+        lastname varchar(15),
+        age int,
+        password varchar(30),
+        email varchar(30),
+        id int
+    );
+SQL
+
+class User
+    @@db = SQLite3::Database.open "db.sql"
+
+    def initialize( firstname, lastname, age, password, email)
+        @firstname = firstname
+        @lastname = lastname
+        @age = age
+        @password = password
+        @email = email
+    end
+
+    def create()
+       id = rand(1..100)
+       @@db.execute("INSERT INTO users (firstname, lastname, age, password, email, id)
+                    VALUES (?,?,?,?,?,?)", [@firstname, @lastname, @age, @password, @email, id])
+        return id
+    end
+
+    def find(param:, val:)
+        results = @@db.query("SELECT * FROM users WHERE #{param}=?", val)
+        return results
+    end
+
+    def find_sanitized(param:, val:)
+        results = @@db.query("SELECT firstname, lastname, age, email, id FROM users WHERE #{param}=?", val)
+        return results
+    end
+
+    def user_auth(email:, password:)
+        results = @@db.query("SELECT 1 FROM users WHERE email=? AND password=?", email, password)
+        return results
+    end
+
+    def find_id(param:, val:)
+        results = @@db.query("SELECT id FROM users WHERE #{param}=?", val)
+        return results
+    end
+
+    def all()
+        results = @@db.query("SELECT * FROM users")
+        return results
+    end
+
+    def update( id:, attribute:, value:)
+        @@db.execute("UPDATE users SET #{attribute} = ? WHERE id=?", value, id)
+        find(db, id:id)
+    end
+
+    def destroy(id:)
+        @@db.execute("DELETE FROM users WHERE id=?", id)
+    end
+
+    
+end
+
+user = User.new("Connor", "Cable", 26,  "1234", "223d@gmail.com")
+user.create
 
 get '/' do
     "Hello World"
 end
 
+get '/users' do
+    results = user.all
+    results.each{ |row| 
+    row.delete_at(3)
+    puts row.join(',')}
+end
+
+post '/users' do
+    @firstname = params["firstname"]
+    @lastname = params["lastname"]
+    @age = params["age"]
+    @password = params["password"]
+    @email = params["email"]
+
+    @user = User.new(@firstname, @lastname, @age, @password, @email)
+    @id = @user.create
+    results = user.find_sanitized(param: "id", val: @id)
+    results.each{ |row| puts row.join(',')}
+end
+
+
+post '/sign_in' do
+    # input is a hash named params
+    # {email: "1234@gmail.com", password:"1234"
+
+    if user.user_auth(email: params["email"], password: params["password"])
+        puts "User Authenticated"
+        @id = user.find_id(param: "password", val: params["password"])
+        session[:user_id] = @id
+        session[:logged_in] = true
+    else
+        puts "User login not found"
+    end
+
+    
+
+
+end
--- fullstack/my_users_app/my_user_model.rb
+++ fullstack/my_users_app/my_user_model.rb
@@ -1,7 +1,6 @@
 require "sqlite3"
 
-db = SQLite3::Database.new "db.sql"
-db.results_as_hash = true
+$db = SQLite3::Database.new "db.sql"
 
 users = db.execute <<-SQL
     create table if not exists users (
@@ -16,35 +15,40 @@ SQL
 
 
 class User
-    def create(db, firstname: , lastname: , age: ,password: ,email:)
+
+    def initialize(db)
+        @@db = db
+    end
+
+    def create(firstname: , lastname: , age: ,password: ,email:)
        id = rand(1..100)
-       db.execute("INSERT INTO users (firstname, lastname, age, password, email, id)
+       @@db.execute("INSERT INTO users (firstname, lastname, age, password, email, id)
                     VALUES (?,?,?,?,?,?)", [firstname, lastname, age, password, email, id])
         return id
     end
 
-    def find(db, id:)
-        results = db.query("SELECT * FROM users WHERE id=?", id)
+    def find(id:)
+        results = @@db.query("SELECT * FROM users WHERE id=?", id)
         return results
         
     end
 
-    def all(db)
-        results = db.query("SELECT * FROM users")
+    def all()
+        results = @@db.query("SELECT * FROM users")
         return results
     end
 
-    def update(db, id:, attribute:, value:)
-        db.execute("UPDATE users SET #{attribute} = ? WHERE id=?", value, id)
+    def update( id:, attribute:, value:)
+        @@db.execute("UPDATE users SET #{attribute} = ? WHERE id=?", value, id)
         find(db, id:id)
     end
 
-    def destroy(db, id:)
-        db.execute("DELETE FROM users WHERE id=?", id)
+    def destroy(id:)
+        @@db.execute("DELETE FROM users WHERE id=?", id)
     end
 
     
 end
 
-user = User.new
-
+curl -X POST -i localhost:8000/users -d "firstname=Tim" -d "lastname=Hortons" -d "age=60" -d "password=1234" -d "email=2123@gmail.com"
+curl POST -i localhost:8000/sign_in -d "email=2123@gmail.com" -d "password=1234" 
