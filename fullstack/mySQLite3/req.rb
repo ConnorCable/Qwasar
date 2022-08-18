d3bb863f09a02be4b85de1d53d03ec99d2ac61b8
ctcmc <ccab0528@gmail.com>
2022-08-17 18:00:20 -0700

    implement update, set, insert,

--- fullstack/mySQLite3/req.rb
+++ fullstack/mySQLite3/req.rb
@@ -5,7 +5,7 @@ class MySqliteRequest
     def initialize()
         @headers = nil
         @table_name = nil
-        @table = []
+        @table = nil
         @select = []
         @where_column = ''
         @where_criteria = ''
@@ -15,16 +15,16 @@ class MySqliteRequest
         @where_results = []
         @select_results = []
         @values = []
+        @query_type = nil
+        @set_data = nil
     end
 
-
+    def query_checker(query)
+        raise "Too many query types" unless @query_type == query
+    end
 
     def from(table_name) # loads a table to @table_name, will append .csv if necessary
-        raise "Table already loaded!!!!" if @table_name  
-        if !(table_name.end_with? ".csv")
-            table_name << ".csv"
-        end
-        @table_name = table_name
+        table_builder(table_name)
         self
     end
 
@@ -38,13 +38,12 @@ class MySqliteRequest
                 headers ||= row.headers
                 @table << row.to_h
         end
-        self
     end
 
     def table_builder(path) # intakes a csv file, outputs an array of hashes corresponding to the row of data. outputs the headers as well
         table = []
         headers = nil
-        CSV.foreach(path, headers: true) do |hash| # iterate through rows of the CSV
+        CSV.foreach(path, headers: true ,header_converters: :symbol) do |hash| # iterate through rows of the CSV
             hash.each do |key, val| # iterate through the hash of the row
                 if Integer(val, exception:false).nil? # if the value can be cast as an integer, do so
                     next
@@ -56,40 +55,37 @@ class MySqliteRequest
         end
         @table = table
         @headers = headers
-        
-        self
     end
 
     def select(*columns) # gets the columns of interest for the run_select column -> is run last after being narrowed by the run_where function
         @select = columns
+        @query_type ||= "select"
+        query_checker("select")
         self
     end
 
-    def run_select()
-            @where_results.each do |row|
-                @select.each do |param|
-                    @select_results.push(row.slice(param.to_sym))
-                end
-            end
-        puts @select_results
-        self
-    end
+    
 
     def where(column, criteria) # gets the column and criteria for the run_where function
         @where_column = column
         @where_criteria = criteria
         self
     end
-
+=begin
     def run_where() # pushes the results of the "where" query to @where_results, which is a narrowed data set from our original table
-        @table.each do |row|
-            if row[@where_column.to_sym] == @where_criteria
-                @where_results.push(row)
+        if @query_type == "select"
+            @table.each do |row|
+                if row[@where_column.to_sym] == @where_criteria
+                    @where_results.push(row)
+                end
             end
+        end
+        if @query_type == "update"
+            
         end
         self
     end
-
+=end
     def order(order, column_name) # order = string, column name = string
         table = @table.sort_by{|hsh| hsh[column_name.to_sym]}
         if order == "asc"
@@ -130,56 +126,140 @@ class MySqliteRequest
     # INSERT INTO students.db VALUES (John, john@johndoe.com, A, https://blog.johndoe.com);
     # https://www.w3schools.com/mysql/mysql_insert.asp
     def insert(table_name)
-        
-        if @values.size == 0
-            raise "No values specified!"
-        end
-        CSV.open(table_name, "w") do |csv|
-            csv << @values
-        end
-
+        # insert function should:
+        # build the table using table_builder, which initialized @table and @headers
+        @table_name = table_name
+        table_builder(table_name)
+        self
+        @query_type ||= "insert"
+        query_checker("insert")
     end
-    def values(data)
-        # validate data
-        table_builder(table_name) ## initializes @headers and @table
-        puts @headers.class
+
+    def values_validator(data)
+        # validate the data
         if data.size != @headers.size
             raise "Not enough headers in your inputted data!"
+        end
+
+        headers_as_strings = []
+        @headers.each_with_index do |element, index|
+            headers_as_strings.push(element.to_s)
         end
         keys = data.keys
         keys.each_with_index do |key, index|
             keys[index] = key.to_s
         end
-        if (keys == @headers)
+        #check if the keys == headers as one string
+        if (keys == headers_as_strings)
             @values = data
         else
             raise "Headers don't match"
         end
-        #@headers.each do |header| # for each header in @headers
-        #end
-        # compare data keys with the keys from @headers
-        # data = (a hash of data on format (key => value)
+
+        if @values.size == 0
+            raise "No values specified!"
+        end
+
+    end
+
+    def values(data)
+        # validate the data
+        values_validator(data)
+        @table.push(data)
+    end
+
+    def update(table_name)
+        @table_name = table_name
+        table_builder(table_name)
+        @query_type ||= "update"
+        query_checker("update")
         self
     end
+
+    # update(table).set({name:Connor, email:ane@janedoe.com, blog: }).where({name:John}).run
+    # UPDATE students SET email = 'jane@janedoe.com', blog = 'https://blog.janedoe.com' WHERE name = 'Jane';
+    def set(data)
+        raise "Set data already loaded" if @set_data
+        flat = data.flatten
+        @set_data = flat[1]
+        @set_column = flat[0]
+        self
+    end
+
+    def delete() 
+        self
+    end
+        
+    def run()
+        case @query_type
+        when "select"
+          run_select
+        when "insert"
+          run_insert
+        when "update"
+          run_update
+        when "delete"
+          run_delete
+        end
+    end
+
+    def run_select()
+        @where_results.each do |row|
+            @select.each do |param|
+                @select_results.push(row.slice(param.to_sym))
+            end
+        end
+        #puts @select_results
+        self
+    end
+
+    def run_insert()
+        temp_header_array = []
+        @headers.each_with_index do |element, index|
+            temp_header_array.push(@headers[index].to_sym)
+        end
+
+        CSV.open("data.csv", "w+") do |csv|
+        csv << temp_header_array
+            @table.each do |hash|
+                csv << hash.values
+            end
+        end
+    end
+
+    # update(table).set({name:Connor, email:ane@janedoe.com, blog: }).where({name:John}).run
+    # {name:Connor, email:ane@janedoe.com, blog: jane@janedoe.com}
+    def run_update()
+        @table.each_with_index do |element, index| # element is a hash
+            if (@table[index][@where_column.to_sym] == @where_criteria)
+                @table[index][@set_column] = @set_data
+            end
+        end
+    end
+
+    def run_delete()
+        # table is an array of hashes table[i] == hash at i index
+        @table.each_with_index do |element, index| # element is a hash
+            if (@table[index][@where_column.to_sym] == @where_criteria)
+                # delete hash row
+                @table.delete_at_index[index]
+            end
+        end
+        puts @table
+    end
+    
+
+    def delete()
+        @query_type = "delete"
+        query_checker("delete")
+    end
 end
 
-csvr = MySqliteRequest.new
+#csvr.delete(ASDasd).from(table).where("year_start", 2017)
+# DELETE FROM students WHERE name = 'John';
+csvr = MySqliteRequest.new()
+csvr.delete.from("mergeddb.csv").where("year_start",2017).run
+#csvr.update("mergeddb.csv").set({height: 999}).where("year_start",2017).run
 #csvr.from("nba_player_data").run_from.select("name", "weight").where("weight", "215").run_where.run_select
 #csvr.from("nba_player_data").join("name","nba_players.csv","Player").run_join.order("asc","weight")
-csvr.values({:name=>"Alaa Abdelnaby", :year_start=>1991, :year_end=>1995, :position=>"F-C", :height=>"6-10", :weight=>240, :birth_date=>"June 24, 1968", :college=>"Duke University"})
-
-testarr = [
-    {name: "connor", age: "0", job: "Coder"},
-    {name: "Josh", age: "23", job: "Dockworker"},
-    {name: "Katy", age: "1000", job: "Student" }
-]
-# test order function
-def order(arr,order, column_name)
-    table = arr.sort_by{|hsh| hsh[column_name.to_sym]}
-    if order == "desc"
-        @table.reverse!
-    end
-    puts table
-end
-
-#order(testarr,"asc","age")
+#csvr.insert("mergeddb.csv").values({"name"=>"Alaa Abdelnaby", "year_start"=>1991, "year_end"=>1995, "position"=>"F-C", "height"=>"6-10", "weight"=>240, "birth_date"=>"June 24, 1968", "college"=>"Duke University"})
